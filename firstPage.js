92f8ba27a90c1f1f251eecf1e910f4f832008efc
rajibhossen <mhossen6311@gmail.com>
2023-12-03 16:42:54 +0600

    deposit tk addition and value show

--- firstPage.js
+++ firstPage.js
@@ -2,14 +2,26 @@
 // Login button event handler
 const loginBtn = document.getElementById('login'); 
 
-loginBtn = addEventListener('click', function() {
+loginBtn.addEventListener('click', function () {
+    
     const loginArea = document.getElementById('login-area');
     loginArea.style.display = 'none';
-
     const transactionArea = document.getElementById('transaction-area');
-
     transactionArea.style.display = "block";
 })
 
 // Deposit button even handler
 const depositBtn = document.getElementById('addDeposit');
+
+depositBtn.addEventListener('click', function(){
+    const depositAmounts = document.getElementById('depositAmount').value;
+    const depositNumber = parseFloat(depositAmounts);
+
+    //add currentDeposit
+    const currentDeposits = document.getElementById('currentDeposit').innerText;
+    const currentDepositNumber = parseFloat(currentDeposits);
+    const totalDeposit = depositNumber + currentDepositNumber;
+     
+    //Card value show
+    document.getElementById('currentDeposit').innerText = totalDeposit;
+})
